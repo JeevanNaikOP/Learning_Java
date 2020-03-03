fc81479938b23f25e609e636588a41e5aec66955
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-03-03 11:44:36 +0530

    Create Maininher.java

---
+++ Maininher.java
@@ -0,0 +1,17 @@
+public class Mainem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Auth a=new Auth();
+		a.uin();
+		a.disp();
+		Employ e=new Employ();
+		e.uin();
+		e.disp();
+		// TODO Auto-generated method stub
+
+	}
+
+}
