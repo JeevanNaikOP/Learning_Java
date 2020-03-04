c461cf545c6cfbcb0dbff29bbb5e16cea2a0927d
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-03-04 12:24:59 +0530

    Create Main.java

---
+++ Interface/Main.java
@@ -0,0 +1,20 @@
+package interf;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Second s =new Second();
+		Third t=new Third();
+		s.input(1011,"au1");
+		
+		
+		t.input(101,"emp");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
