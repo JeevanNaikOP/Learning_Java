2e4432124c35ef9eb2e0dff5e02597cf4815e62a
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-03-09 23:38:05 +0530

    Create Twoor.java

---
+++ codingBat/Twoor.java
@@ -0,0 +1,11 @@
+//Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are ther
+
+public String front22(String str) {
+   int take = 2;
+  if (take > str.length()) {
+    take = str.length();
+  }
+  
+  String front = str.substring(0, take);
+  return front + str + front;
+}
