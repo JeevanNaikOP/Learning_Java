d79fb6ab66cd751801d5a74d1585020330bb01d4
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-03-19 19:55:58 +0530

    Update lastChars.java

--- codingBat/lastChars.java
+++ codingBat/lastChars.java
@@ -1,10 +1,10 @@
 
-Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+//Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
 
 
-lastChars("last", "chars") → "ls"
-lastChars("yo", "java") → "ya"
-lastChars("hi", "") → "h@"
+//lastChars("last", "chars") → "ls"
+//lastChars("yo", "java") → "ya"
+//lastChars("hi", "") → "h@"
 
 public String lastChars(String a, String b) {
    if((a.length()==0)&&(b.length()==0))return("@@");
