6baca3681f0c5903f3e8ec064ffc394fe2543e5b
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-04-01 09:52:26 +0530

    Create  lessBy10.java

---
+++ codingBat/lessBy10.java
@@ -0,0 +1,19 @@
+
+/*Given three ints, a b c, return true if one of them is 10 or more less than one of the others.
+
+
+lessBy10(1, 7, 11) → true
+lessBy10(1, 7, 10) → false
+lessBy10(11, 1, 7) → true
+*/
+
+  
+public boolean lessBy10(int a, int b, int c) {
+  int high = Math.max(a,b);
+  high = Math.max(high, c);
+  
+  if (high - a >= 10 || high - b >= 10 || high - c >=10)
+    return true;
+  else 
+    return false;
+}
