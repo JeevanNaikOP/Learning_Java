95d2d3de32619570734660bf651bb83c6ce4ec3c
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-19 11:55:35 +0530

    Update scoresAverage.java

--- codingBat/scoresAverage.java
+++ codingBat/scoresAverage.java
@@ -17,7 +17,7 @@ public int scoresAverage(int[] scores) {
   int avg1 = average(scores, 0, scores.length/2);
   int avg2 = average(scores, scores.length/2, scores.length);
   
-  return Math.max(avg1,avg2) 
+  return Math.max(avg1,avg2); 
   
 }
 
