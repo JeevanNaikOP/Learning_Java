9cd0a957fed77e6983afddf25912de8ed744eedd
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-21 16:00:13 +0530

    Update copyEvens.java

--- codingBat/copyEvens.java
+++ codingBat/copyEvens.java
@@ -5,6 +5,7 @@ The original array will contain at least "count" even numbers.
 copyEvens([3, 2, 4, 5, 8], 2) → [2, 4]
 copyEvens([3, 2, 4, 5, 8], 3) → [2, 4, 8]
 copyEvens([6, 1, 2, 4, 5, 8], 3) → [6, 2, 4]
+*/
 
 
 public int[] copyEvens(int[] nums, int count) {
