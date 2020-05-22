6817b7700e06d2d8f2fc100e1a706ceb22e10935
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-22 12:21:37 +0530

    Update matchUpap1.java

--- codingBat/matchUpap1.java
+++ codingBat/matchUpap1.java
@@ -1,4 +1,4 @@
-Given 2 arrays that are the same length containing strings, compare the 1st string in one array to the 1st string in the other array,
+/*Given 2 arrays that are the same length containing strings, compare the 1st string in one array to the 1st string in the other array,
 the 2nd to the 2nd and so on. Count the number of times that the 2 strings are non-empty and start with the same char. 
 The strings may be any length, including 0.
 
@@ -6,6 +6,7 @@ The strings may be any length, including 0.
 matchUp(["aa", "bb", "cc"], ["aaa", "xx", "bb"]) → 1
 matchUp(["aa", "bb", "cc"], ["aaa", "b", "bb"]) → 2
 matchUp(["aa", "bb", "cc"], ["", "", "ccc"]) → 1
+*/
 
 public int matchUp(String[] a, String[] b) {
   int count = 0;
